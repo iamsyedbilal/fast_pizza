49670e29b057952e1cf39caf74788d75123eac6d
iamsyedbilal <syedbilal.dev27@gmail.com>
2026-04-05 15:45:20 +0500

    Add routing and cart functionality with new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './ui/Home'
+import Menu from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import CreateOrder from './features/order/CreateOrder'
+import Order from './features/order/Order'
+import AppLayout from './ui/AppLayout'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      { path: '/menu', element: <Menu /> },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:id',
+        element: <Order />,
+      },
+    ],
+  },
+])
+
 export default function App() {
-  return <div>App</div>
+  return <RouterProvider router={router} />
 }
